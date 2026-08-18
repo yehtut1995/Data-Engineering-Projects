5ffe90cdce8cb5d6a3502ad95a6552e6facccaa3
yehtut <dr.physics.95@gmail.com>
2026-08-18 16:06:58 +0700

    Add Snowflake and dbt ETL pipeline

---
+++ 2. Snowflake & dbt- ETL Pipeline and Data Modeling for Analytics Engineering/models/staging/stg_tpch_orders.sql
@@ -0,0 +1,7 @@
+SELECT 
+    o_orderkey as order_key,
+    o_custkey as customer_key,
+    o_orderstatus as order_status,
+    o_totalprice as total_price,
+    o_orderdate as order_date
+FROM {{ source('tpch', 'orders') }}
